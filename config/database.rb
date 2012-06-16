fe6e2842b8917dd7b510e08d1617e19eee956e86
Stef Lewandowski <stef@stef.io>
2012-06-16 15:35:18 +0100

    pool timeout and css

--- config/database.rb
+++ config/database.rb
@@ -9,7 +9,7 @@ case Padrino.env
     Mongoid.database = connection.db("silly-markov")
   when :production then
     uri  = URI.parse(ENV['MONGO_PRODUCTION_URI'] || ENV['MONGOLAB_URI'])
-    connection = Mongo::Connection.from_uri(ENV['MONGO_PRODUCTION_URI'] || ENV['MONGOLAB_URI'], {:pool_timeout => 60, :pool_size => 5, :timeout => 10})
+    connection = Mongo::Connection.from_uri(ENV['MONGO_PRODUCTION_URI'] || ENV['MONGOLAB_URI'])
     Mongoid.database = connection.db(uri.path.gsub(/^\//, ''))
 end
 
